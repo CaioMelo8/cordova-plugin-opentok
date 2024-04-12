b0a599c189ccb543eef30e362ba3a284ef682dde
Caio Melo <caiomelo8@gmail.com>
2024-04-12 13:22:18 -0300

    Update OpenTok SDK.

--- src/ios/OpenTokPlugin.m
+++ src/ios/OpenTokPlugin.m
@@ -57,7 +57,7 @@
     [payload setObject:@"3.5.1" forKey:@"cp_version"];
     NSMutableDictionary *logData = [[NSMutableDictionary alloc]init];
     [logData setObject:apiKey forKey:@"partner_id"];
-    [logData setObject:@"2.22.3" forKey:@"build"];
+    [logData setObject:@"2.27.2" forKey:@"build"];
     [logData setObject:@"https://github.com/opentok/cordova-plugin-opentok" forKey:@"source"];
     [logData setObject:@"info" forKey:@"payload_type"];
     [logData setObject:payload forKey:@"payload"];
